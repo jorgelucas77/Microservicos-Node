df3719a563ce4d29284d690f0a48c200f0aaab54
Jorge Lucas <jorgelucas77@gmail.com>
2021-10-15 10:53:13 +0200

    Projeto final

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 
-import express, { Request, Response, NextFunction} from 'express';
+import express from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Sucesso! Vocês são de mais!!'});
-});
+//configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
+//configurações de Rotas
+app.use(usersRoute);
+app.use(statusRoute);
+
+//Inicialização do Servidor
 app.listen(3000, () => {
     console.log('Aplicação a correr na porta 3000!');
 });
