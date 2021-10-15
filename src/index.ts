e8bbf434506b930e52f5f19bad7a4555b2ef3bfe
Jorge Lucas <jorgelucas77@gmail.com>
2021-10-15 09:56:34 +0200

    Adicionado o typescript e incrementando a forma de desenvolver

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 
-console.log('Executei!');
+import express, { Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'Sucesso! Vocês são de mais!!'});
+});
+
+app.listen(3000, () => {
+    console.log('Aplicação a correr na porta 3000!');
+});
